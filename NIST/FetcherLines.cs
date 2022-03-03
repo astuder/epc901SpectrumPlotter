b48be0728aa861b6ac207f368557858c58755c34
Adrian Studer <github@adrianstuder.com>
2022-03-02 20:45:01 -0800

    use strength instead of instensity for NIST lines

--- NIST/FetcherLines.cs
+++ NIST/FetcherLines.cs
@@ -12,9 +12,9 @@ namespace SpectrumPlotter.NIST
     {
         public static int MinWavelength = 300;
         public static int MaxWavelength = 1100;
-        public static int MinRelIntensity = 100;
+        public static int MinTransitionStrength = 1;
         public static int MaxLevel = 2;
-
+     
         private static string RemoveNotes(string str)
         {
             int pos = 0;
@@ -39,8 +39,9 @@ namespace SpectrumPlotter.NIST
         {
             try
             {
-                string url = "https://physics.nist.gov/cgi-bin/ASD/lines1.pl?spectra=" + element + "&limits_type=0&low_w=" + MinWavelength + "&upp_w=" + MaxWavelength + "&unit=1&de=0&format=3&line_out=0&remove_js=on&en_unit=0&output=0&bibrefs=1&page_size=15&show_obs_wl=1&order_out=0&max_low_enrg=&show_av=2&max_upp_enrg=&tsb_value=0&min_str=&A_out=0&intens_out=on&max_str=&allowed_out=1&forbid_out=1&min_accur=&min_intens=" + MinRelIntensity + "&submit=Retrieve+Data";
-
+                // string url = "https://physics.nist.gov/cgi-bin/ASD/lines1.pl?spectra=" + element + "&limits_type=0&low_w=" + MinWavelength + "&upp_w=" + MaxWavelength + "&unit=1&de=0&format=3&line_out=0&remove_js=on&en_unit=0&output=0&bibrefs=1&page_size=15&show_obs_wl=1&order_out=0&max_low_enrg=&show_av=2&max_upp_enrg=&tsb_value=0&min_str=&A_out=0&intens_out=on&max_str=&allowed_out=1&forbid_out=1&min_accur=&min_intens=" + MinRelIntensity + "&submit=Retrieve+Data";
+                string url = "https://physics.nist.gov/cgi-bin/ASD/lines1.pl?spectra=" + element + "&limits_type=0&low_w=" + MinWavelength + "&upp_w=" + MaxWavelength + "&unit=1&de=0&format=3&line_out=0&remove_js=on&en_unit=0&output=0&page_size=15&show_obs_wl=1&order_out=0&max_low_enrg=&show_av=2&max_upp_enrg=&tsb_value=0&min_str=" + MinTransitionStrength + "&A_out=0&intens_out=on&max_str=&allowed_out=1&forbid_out=1&min_accur=&min_intens=&J_out=on&submit=Retrieve+Data";
+                
                 HttpClient client = new HttpClient();
 
                 Task<HttpResponseMessage> ret = client.GetAsync(url);
@@ -72,9 +73,9 @@ namespace SpectrumPlotter.NIST
                 int max_level = 1;
                 int[] levels = new int[rows.Length-2];
                 double[] wavelengths = new double[rows.Length-2];
-                int[] intensities = new int[rows.Length-2];
+                // int[] intensities = new int[rows.Length-2];
+                double[] intensities = new double[rows.Length-2];
 
-                
                 for (int i = 0; i < rows.Length-2; i++)
                 {
                     string[] cols = rows[i+1].Split('\t');
@@ -86,7 +87,8 @@ namespace SpectrumPlotter.NIST
                         if (levels[i] > max_level) max_level = levels[i];
                     }
                     wavelengths[i] = double.Parse(cols[data_pos]);
-                    intensities[i] = Int32.Parse(RemoveNotes(cols[data_pos + 1]));
+                    // intensities[i] = Int32.Parse(RemoveNotes(cols[data_pos + 1]));
+                    intensities[i] = double.Parse(cols[data_pos+2]);
                 }
 
                 ElementInfo spectra = new ElementInfo(element);
@@ -122,61 +124,6 @@ namespace SpectrumPlotter.NIST
                         lvl_index++;
                     }
                 }
-
-                /*
-                string assignStart = responseBody.Substring(responseBody.IndexOf("var dataDopplerArray=") + 21);
-                string assignContent = assignStart.Substring(0, assignStart.IndexOf("    var dataSticksArray=")).Replace("\r", "").Replace("\n", "").Trim(';');
-
-                JArray data = JsonConvert.DeserializeObject(assignContent) as JArray;
-                
-                List<double> wavelengths = new List<double>();
-                List<List<double>> elementIntensities = new List<List<double>>();
-
-                JArray header = data[0] as JArray;
-                int elementStart = 1;
-
-                for (int pos = 1; pos < header.Count; pos++)
-                {
-                    JObject elementInfo = header[pos] as JObject;
-                    string name = elementInfo["label"].ToString();
-
-                    if (name.StartsWith("Sum"))
-                    {
-                        elementStart = 2;
-                    }
-                    else
-                    {
-                        spectra.AddElement(name);
-                        elementIntensities.Add(new List<double>());
-                    }
-                }
-                int elements = header.Count - elementStart;
-
-                for (int pos = 1; pos < data.Count; pos++)
-                {
-                    JArray spectrumInfo = data[pos] as JArray;
-                    double wavelength = ((double)spectrumInfo[0]);
-                    double sum = ((double)spectrumInfo[1]);
-
-                    wavelengths.Add(wavelength);
-                    for (int posw = 0; posw < elements; posw++)
-                    {
-                        double intensity = 0;
-
-                        if (spectrumInfo[elementStart + posw].Type == JTokenType.Float)
-                        {
-                            intensity = ((double)spectrumInfo[elementStart + posw]);
-                        }
-                        elementIntensities[posw].Add(intensity);
-                    }
-                }
-
-                spectra.Wavelengths = wavelengths.ToArray();
-                for (int pos = 0; pos < elements; pos++)
-                {
-                    spectra.Elements[pos].Intensities = elementIntensities[pos].ToArray();
-                }
-*/
 
                 spectra.Save("Lines");
 
